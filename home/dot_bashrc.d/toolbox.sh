8f14d1f3459befb91bb44db85c1e270bdd45828b
Germán Molins <germanmolins@gmail.com>
2024-11-12 13:18:17 +0100

    feat(bash): function to print list of CLI tools

---
+++ home/dot_bashrc.d/toolbox.sh
@@ -0,0 +1,14 @@
+toolbox ()
+{
+	{
+		echo 'atuin'
+		echo 'git alias'
+		echo 'gitsh     : git sh'
+		echo 'rename'
+		echo 'task'
+		echo 'tasknote  : task <id> note'
+		echo 'tasksh    : task sh'
+		echo 'timew'
+		echo 'qmv (renameutils)'
+	} | sort -u
+}
