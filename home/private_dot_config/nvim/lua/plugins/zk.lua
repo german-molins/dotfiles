959222f0053c12a197aa78810f2fd6737b7e7631
german-molins <germanmolins@gmail.com>
2025-02-11 22:32:43 +0100

    feat(nvim): add plugin zk-nvim

---
+++ home/private_dot_config/nvim/lua/plugins/zk.lua
@@ -0,0 +1,20 @@
+return {
+  "zk-org/zk-nvim",
+  config = function()
+    require("zk").setup({
+      picker = "fzf_lua",
+
+      lsp = {
+        config = {
+          cmd = { "zk", "lsp" },
+          name = "zk",
+        },
+
+        auto_attach = {
+          enabled = true,
+          filetypes = { "markdown" },
+        },
+      },
+    })
+  end,
+}
