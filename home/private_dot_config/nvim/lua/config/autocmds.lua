8e9f0a6cc8e7eb57b5492b41ee1388e8418de0c7
Germán Molins <germanmolins@gmail.com>
2025-02-26 13:03:34 +0100

    feat(nvim): add conditional formatting for text files in diff mode
    Enhance text file formatting to respect diff mode by conditionally
    appending 'a' format option only when not in diff view.

--- home/private_dot_config/nvim/lua/config/autocmds.lua
+++ home/private_dot_config/nvim/lua/config/autocmds.lua
@@ -6,9 +6,16 @@ local TEXT_FILETYPES = {
   "markdown",
 }
 
+--[[ 
+Enable automatic formatting ('a' option) for text files,
+but only when not in diff mode to preserve diff view formatting.
+]]
+--
 vim.api.nvim_create_autocmd("FileType", {
   pattern = TEXT_FILETYPES,
   callback = function()
-    vim.opt.formatoptions:append("a")
+    if not vim.o.diff then
+      vim.opt.formatoptions:append("a")
+    end
   end,
 })
