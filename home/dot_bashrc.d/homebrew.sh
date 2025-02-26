f8e9f48d548a1fce517ab3bb80d241c2360ddd8e
Germán Molins <germanmolins@gmail.com>
2025-02-26 22:40:01 +0100

    fix(brew): enable bash completion for installed packages
    Following doc of `bash-completion@2`, source bash completion script for
    autocompletion of apps installed by homebrew.

--- home/dot_bashrc.d/homebrew.sh
+++ home/dot_bashrc.d/homebrew.sh
@@ -1,2 +1,6 @@
 # Set shell env.
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv 2>/dev/null)"
+
+if [ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]; then
+  . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
+fi
