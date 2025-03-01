5b7bfc2389ab6d43b5d646376a63a744230d52ef
Germán Molins <germanmolins@gmail.com>
2025-03-01 17:09:03 +0100

    [WIP]fix(nvim): add autocmd to disable autoformat for markdown
    Uses treesitter to disable autoformat if inside nodes of type
    
    - code fence
    - quote block
    - frontmatter
    
    Howerer, it does not work.

--- home/private_dot_config/nvim/lua/config/autocmds.lua
+++ home/private_dot_config/nvim/lua/config/autocmds.lua
@@ -19,3 +19,36 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
 })
+
+local ts_utils = require("nvim-treesitter.ts_utils")
+
+-- Function to check if the cursor is in a specific Treesitter node type
+local function is_cursor_in_node_type(node_types)
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    local type = node:type()
+    if vim.tbl_contains(node_types, type) then
+      return true
+    end
+    node = node:parent()
+  end
+  return false
+end
+
+-- Function to toggle formatoptions based on cursor position
+local function toggle_formatoptions()
+  local node_types = { "code_fence_content", "quote_block", "frontmatter" }
+  if is_cursor_in_node_type(node_types) then
+    vim.bo.formatoptions = vim.bo.formatoptions:gsub("a", "") -- Disable autoformatting
+  else
+    if not vim.bo.formatoptions:match("a") then
+      vim.bo.formatoptions = vim.bo.formatoptions .. "a" -- Re-enable autoformatting
+    end
+  end
+end
+
+-- Set up an autocommand to toggle formatoptions on cursor movement
+vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+  pattern = "markdown",
+  callback = toggle_formatoptions,
+})
