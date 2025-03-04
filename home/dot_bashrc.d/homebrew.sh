2940eafcbc6eab760e0ad97480d8fdfce23ee32c
Germán Molins <germanmolins@gmail.com>
2025-03-04 15:18:44 +0100

    feat(bash,brew): add support for macOS
    Correct brew prefix path for different operating systems and
    architectures (amd and arm).

--- home/dot_bashrc.d/homebrew.sh
+++
@@ -1,6 +0,0 @@
-# Set shell env.
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv 2>/dev/null)"
-
-if [ -r "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]; then
-  . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"
-fi
