e2fc7986c40d25795d8c068581d81748920de8b8
Germán Molins <germanmolins@gmail.com>
2025-04-09 17:24:17 +0200

    refactor(nvim,lazyvim): move extras to config and untrack lazyvim.json
    Declare extras in config instead. They are not managed by `:LazyExtras`
    anymore. Untracking lazyvim.json allows interactive management of
    `:LazyExtras` without conflicts in `chezmoi status`.

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -18,6 +18,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     -- import/override with your plugins
     { import = "plugins" },
   },
