563e996288ddecfbc052cc27606edc0133191c7b
Germán Molins <germanmolins@gmail.com>
2025-04-10 11:54:27 +0000

    refactor(nvim,lazivim): add extra plugins `dap.core` and `dap.nlua`
    DAP: Debug Adapter Protocol

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -28,6 +28,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     -- import/override with your plugins
     { import = "plugins" },
   },
