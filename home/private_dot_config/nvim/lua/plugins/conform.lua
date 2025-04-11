dcbb7d55737e562833c3690bf18277d24131b623
Germán Molins <germanmolins@gmail.com>
2025-04-11 11:23:02 +0200

    feat(lazyvim,conform): set `shfmt` as the shell filetype formatter
    Override default settings by appending (not prepending) command line args.

--- home/private_dot_config/nvim/lua/plugins/conform.lua
+++ home/private_dot_config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,17 @@ return {
   opts = {
     formatters_by_ft = {
       typescript = { "prettier" },
+      sh = { "shfmt" },
+    },
+    formatters = {
+      shfmt = {
+        append_args = {
+          "-i=4", -- indentation
+          "-ci", -- indent switch cases
+          "-fn", -- function next line
+          "-bn", -- allow binary operators beginning of line
+        },
+      },
     },
   },
 }
