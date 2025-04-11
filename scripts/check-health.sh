374e48cc56fa039fced645c5418e7cdc4adf074f
Germán Molins <germanmolins@gmail.com>
2025-04-11 14:53:01 +0200

    chore(refactor): encapsulate contributor checks in functions
    Refactor the script to encapsulate contributor checks in dedicated functions.
    Add a check for matching personal email with the contributor's email.
    
    Improve health check script with explicit conditionals
    
    Add git user email check: Implement a function to verify that the Git
    user email matches the personal email, enhancing the health check
    process.
    
    Improve error handling and output in check-health script

--- scripts/check-health.sh
+++ scripts/check-health.sh
@@ -7,27 +7,63 @@
 # matches the personal email from chezmoi config.
 ###
 
+check_contributors()
+(
+    local contributors_emails="$1"
+    local num_contributors="$(echo "$contributors_emails" | wc -l | xargs)"
+
+    if [ "$num_contributors" -gt 1 ]; then
+        >&2 printf "ERROR: There are more than one contributors ($num_contributors) to"
+        >&2 printf " this repository:\n"
+        >&2 echo "$contributors_emails"
+
+        exit 1
+    else
+        >&2 printf "INFO: Only contributor ($num_contributors) to this repository:\n"
+        >&2 echo "$contributors_emails"
+    fi
+)
+
+check_email_match()
+(
+    local email="$1"
+    local contributor="$2"
+
+    if [ "$email" != "$contributor" ]; then
+        >&2 printf "ERROR: The contributor email $contributor does not match the personal"
+        >&2 printf " email $email in chezmoi data.\n"
+
+        exit 1
+    else
+        >&2 printf "INFO: Contributor email matches the personal email $email"
+        >&2 printf " in chezmoi data.\n"
+    fi
+)
+
+check_git_user_email()
+(
+    local personal_email="$1"
+    local git_user_email="$(git config user.email)"
+
+    if [ "$git_user_email" != "$personal_email" ]; then
+        printf "ERROR: Git user email $git_user_email does not match the personal"
+        printf " email $personal_email.\n"
+
+        exit 1
+    else
+        >&2 printf "INFO: Git user email matches the personal email $personal_email"
+        >&2 printf " in chezmoi data.\n"
+    fi
+)
+
 contributors_emails="$(git log --format='%ae' | sort -u)"
-num_contributors="$(echo "$contributors_emails" | wc -l | xargs)"
-
-if [ "$num_contributors" -gt 1 ]; then
-    echo -n "ERROR: There are more than one contributors ($num_contributors) to"
-    echo " this repository:"
-    echo "$contributors_emails"
-    exit 1
-else
-    contributor="$(echo "$contributors_emails")"
-    unset contributors
-fi
-
 personal_email="$(chezmoi data | jq .personal_email --raw-output)"
 
-if [ "$personal_email" != "$contributor" ]; then
-    echo -n "ERROR: The contributor email $contributor does not match the personal"
-    echo " email $personal_email in chezmoi data."
-    exit 1
-else
-    echo "INFO: Only contributor to this repository: $contributor (verified)"
+if check_contributors "$contributors_emails"; then
+    contributor="$(echo "$contributors_emails")"
+    check_email_match "$personal_email" "$contributor"
 fi
+
+check_git_user_email "$personal_email"
 
 exit 0
