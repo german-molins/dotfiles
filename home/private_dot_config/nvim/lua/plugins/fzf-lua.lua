759fa86122e23bba3b761dc3ff52bddf4ba3f6a2
Germán Molins <germanmolins@gmail.com>
2025-05-08 15:14:28 +0200

    feat(nvim,fzf): set preview layout to "flex"
    Use "horizontal" layout for screens smaller than 200 columns width, and
    "vertical" layout otherwise.

--- home/private_dot_config/nvim/lua/plugins/fzf-lua.lua
+++ home/private_dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -3,6 +3,10 @@ return {
   opts = {
     winopts = {
       fullscreen = true,
+      preview = {
+        layout = "flex",
+        flip_columns = 200, -- cols to switch to horizontal on flex
+      },
     },
   },
 }
