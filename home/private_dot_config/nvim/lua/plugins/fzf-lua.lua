630ee60e2ccd11b45a91d19c2dbc6a3144e514e6
Germán Molins <germanmolins@gmail.com>
2025-05-09 16:48:18 +0200

    feat(nvim,fzf): increase num of cols to flip preview in "flex" layout
    Even larger displays truncate long paths in monorepo projects.

--- home/private_dot_config/nvim/lua/plugins/fzf-lua.lua
+++ home/private_dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -5,7 +5,7 @@ return {
       fullscreen = true,
       preview = {
         layout = "flex",
-        flip_columns = 200, -- cols to switch to horizontal on flex
+        flip_columns = 250, -- cols to switch to horizontal on flex
       },
     },
   },
