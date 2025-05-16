254e2353b20b5e71398feb44d95aa3b2bd3dbd75
Germán Molins <germanmolins@gmail.com>
2025-05-16 18:59:55 +0200

    feat(nvim,color,neosolarized): explicit defaults for style of syntax groups

--- home/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,15 @@ return {
       -- Italics for different hightlight groups (eg. Statement, Condition,
       -- Comment, Include, etc.)
       enable_italics = false,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+        functions = { bold = true },
+        variables = {},
+        string = {
+          italic = true,
+        },
+      },
     },
   },
 
