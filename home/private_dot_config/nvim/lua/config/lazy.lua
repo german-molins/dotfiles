ccd2081515f88212463087d9983b4de7a97df1d2
Germán Molins <germanmolins@gmail.com>
2025-05-17 00:35:35 +0200

    build(lazyvim): add extra plugin `ui.indent-blankline`
    Though it was clearly enabled before this, but needed to explicitly add
    it in order to use `on_highlight` color overrides on its color types in
    `NeoSolarized`.

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.ui.indent-blankline" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     -- import/override with your plugins
