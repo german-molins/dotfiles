87d16d7fd0bb571b6da2213958dd649d9a70c233
Germán Molins <germanmolins@gmail.com>
2025-05-27 16:37:18 +0200

    refactor(nvim,snacks-picker): include explicit default for layout dynamic selection
    For future modification. It is equivalent to the current setting of `flip_columns = 250`
    of `fzf-lua`.

--- home/private_dot_config/nvim/lua/plugins/snacks-picker.lua
+++ home/private_dot_config/nvim/lua/plugins/snacks-picker.lua
@@ -5,6 +5,11 @@ return {
     picker = {
       layout = {
         fullscreen = true,
+        cycle = true,
+        --- Use the default layout or vertical if the window is too narrow
+        preset = function()
+          return vim.o.columns >= 120 and "default" or "vertical"
+        end,
       },
     },
   },
