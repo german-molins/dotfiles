bdda7b69553f9f03fc49d5622ec57faa1cce092a
Germán Molins <germanmolins@gmail.com>
2025-07-16 13:26:37 +0200

    refactor(bash,task): make function name "private" by prefix naming convention
    Still visible, but means hidden.

--- home/dot_bashrc.d/55-task.sh
+++ home/dot_bashrc.d/55-task.sh
@@ -10,14 +10,14 @@ function task
             tasksh
         elif [ $1 = stop ]; then
             # Stop the first active task.
-            id=$(get_active_task)
+            id=$(__task_get_active_task)
             TASK_LAST="$id"
             command task "$id" stop
         elif [ $1 = continue ]; then
             # Continue the last task stopped with no args.
             command task "$TASK_LAST" start
         elif [ $1 = done ]; then
-            id=$(get_active_task)
+            id=$(__task_get_active_task)
             TASK_LAST="$id"
             # Done the last task stopped with no args.
             command task "$TASK_LAST" done
@@ -40,7 +40,7 @@ function task
     fi
 }
 
-function get_active_task
+function __task_get_active_task
 {
     task active | sed -n '/ID/{n;n;p;}' | awk '{print $1}'
 }
