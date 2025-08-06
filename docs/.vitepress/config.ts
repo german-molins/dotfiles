998b94c2d1b0962e5b36e15631f7167b5c3d019b
Germán Molins <germanmolins@gmail.com>
2025-08-06 15:19:25 +0200

    build(mise-global): add packages `resvg` and `ffmpeg`
    System dependencies of the environment

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
             collapsed: true,
             items: [
               { text: "Mise", link: '/tools/mise' },
+              { text: "System Dependencies", link: '/tools/system_dependencies' },
               { text: "Git", link: '/tools/git' },
               { text: "Neovim", link: '/tools/nvim' },
               { text: "http", link: '/tools/http' },
---
+++ docs/tools/system_dependencies.md
@@ -0,0 +1,9 @@
+# System Dependencies
+
+- `ffmpeg` (mise env `opt`):
+  - Yazi: Video thumnails
+- `resvg` (mise env `opt`):
+  - Neovim: Plugin `mdmath`
+  - Yazi: Preview SVG images
+- ImageMagick (mise env `nix`):
+  - Yazi: Image formats preview
