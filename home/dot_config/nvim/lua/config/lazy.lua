868ebb831265de610d20bd4201ba8bba9b14c057
Germán Molins <germanmolins@gmail.com>
2026-06-23 18:15:13 +0200

    build(nvim): add lazygit extra lang.clangd
    Ref: https://www.lazyvim.org/extras/lang/clangd

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.ui.indent-blankline" },
